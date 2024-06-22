bed00b2b73dd475b6c122ca1ee33fc56d44ec80d
thomasfahy <thomasfahy11@gmail.com>
2024-06-22 17:03:03 +0100

    feat: allows creatgrid() to build divs inside divs
    This is to create the 4x4 layout required by the grid

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const gridSize = 4;
+
 function createGrid(){
-    mainContainer = document.querySelector(".mainContainer");
+    const mainContainer = document.querySelector(".mainContainer");
+    for (i=0; i < gridSize; i++){
+        const containerDiv = document.createElement('div');
+        containerDiv.className = "containerDiv";
+        mainContainer.appendChild(containerDiv);
+        for (j=0; j < gridSize; j++){
+            const gridDiv = document.createElement('div');
+            gridDiv.className = "gridDiv";
+            containerDiv.appendChild(gridDiv);
+        }
+    }
 }
+createGrid();
