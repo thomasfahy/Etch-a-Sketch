23547a5164336d4af845f777aa315698a96cb8ec
thomasfahy <thomasfahy11@gmail.com>
2024-06-22 17:15:56 +0100

    feat: add styling to grid div elements
    this allows the grid to become visible on the page

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const gridSize = 4;
 
 function createGrid(){
     const mainContainer = document.querySelector(".mainContainer");
-    for (i=0; i < gridSize; i++){
+    for (let i=0; i < gridSize; i++){
         const containerDiv = document.createElement('div');
         containerDiv.className = "containerDiv";
         mainContainer.appendChild(containerDiv);
-        for (j=0; j < gridSize; j++){
+        for (let j=0; j < gridSize; j++){
             const gridDiv = document.createElement('div');
             gridDiv.className = "gridDiv";
             containerDiv.appendChild(gridDiv);
